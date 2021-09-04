a99a3a09d2b24c2fea787426b0d776d952fbd4d5
CodeThales <thalles.agency@gmail.com>
2021-09-04 14:41:00 -0300

    Update Swagger Authentication

--- BlueMusicBearerAutToken/Startup.cs
+++ BlueMusicBearerAutToken/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -71,6 +72,34 @@ namespace BlueMusicBearerAutToken
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = $"{Path.Combine(AppContext.BaseDirectory, xmlFile)}";
                 c.IncludeXmlComments(xmlPath);
+
+                #region Swagger Bearer Authentication
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \n Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+                #endregion
             });
             #endregion
 
